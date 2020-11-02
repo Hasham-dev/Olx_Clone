26a13ca1c00680568293967080930ac1a29220f5
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-11-02 23:31:19 +0500

    OLX Clone

---
+++ build/precache-manifest.7f8a145bbf1d394d50bbb64ee2ef2657.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f55b7cbbdbd1bb564026e1a91dc837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef1293d4c31f18c3d58b",
+    "url": "/static/css/main.48c6bed2.chunk.css"
+  },
+  {
+    "revision": "75581c269bfccc58d840",
+    "url": "/static/js/2.b4415707.chunk.js"
+  },
+  {
+    "revision": "ef1293d4c31f18c3d58b",
+    "url": "/static/js/main.80850287.chunk.js"
+  },
+  {
+    "revision": "04bbcf2b368f274741a0",
+    "url": "/static/js/runtime-main.a10293b3.js"
+  }
+]);
